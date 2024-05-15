d92f16d5b01db523569661dc9b0ad2521ca1f583
Abass Ibrahim <abassdibrahim@gmail.com>
2024-05-15 01:16:23 +0100

    configured lenis with smooth scrolling

--- verityx-website/src/app/page.js
+++ verityx-website/src/app/page.js
@@ -1,9 +1,18 @@
-const Home = async() => {
+const Home = async () => {
+  const data = [...Array(100).keys()];
+
   return (
     <>
       <section className="h-screen flex justify-center">
         <div className="w-full flex flex-col items-center">
           <h1>Hello world</h1>
+        </div>
+      </section>
+      <section>
+        <div className="flex flex-col gap-20">
+          {data.map((item, index) => (
+            <p>{item}</p>
+          ))}
         </div>
       </section>
     </>
