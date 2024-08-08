35c35255edcd4ccbd9fa32b7baf3114d9255065b
Abass Ibrahim <abassdibrahim@gmail.com>
2024-08-08 13:37:29 +0100

    installed mdx

--- verityx-website/next.config.mjs
+++ verityx-website/next.config.mjs
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from "@next/mdx";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  trailingSlash: true,
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "woyxacfrzfhkyahgfwpw.supabase.co",
+      },
+    ],
+  },
+};
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+});
+
+export default withMDX(nextConfig);
