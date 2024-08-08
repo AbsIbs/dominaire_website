cfea1bffc2cfc61cbec511a9298d45b61fe9e0c2
Abass Ibrahim <abassdibrahim@gmail.com>
2024-08-08 14:00:50 +0100

    mdx files can be read from supabase

--- verityx-website/src/app/projects/[name]/page.js
+++ verityx-website/src/app/projects/[name]/page.js
@@ -1,5 +1,6 @@
 // Components
 import Header from "./components/header";
+import { MDXRemote } from "next-mdx-remote/rsc";
 // Supabase
 import { createClient } from "../../../../utils/supabase/server.ts";
 // Logic
@@ -15,12 +16,19 @@ const Page = async ({ params }) => {
     .eq(`client.name`, formatClientTitle(params.name))
     .single();
 
+  // Fetch summary
+  const res = await fetch(
+    `https://woyxacfrzfhkyahgfwpw.supabase.co/storage/v1/object/public/summaries/${params.name}/summary.mdx`
+  );
+  const mdxText = await res.text();
+
   return (
     <>
       <section className="xpadding py-16">
         <div className="flex gap-4 items-center justify-center">
           <div className="flex flex-col ~sm/lg:~gap-12/24 w-[1440px]">
             <Header data={data} />
+            <MDXRemote source={mdxText} />
           </div>
         </div>
       </section>
